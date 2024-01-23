cb4d4b0085c71e7d2de41f9a0007c5e91a94001d
a2nfinance <levi@a2n.finance>
2024-01-23 13:48:58 +0700

    update e2e tests: create DAO and check DAO info

--- contracts/dao_factory/lib.rs
+++ contracts/dao_factory/lib.rs
@@ -5,6 +5,7 @@ mod dao_factory {
     use dao::{DaoRef, Step};
     use ink::prelude::vec::Vec;
     use ink::ToAccountId;
+    use ink::prelude::string::String;
     #[ink(storage)]
     pub struct DaoFactory {
         owner: AccountId,
@@ -84,7 +85,6 @@ mod dao_factory {
 
             let dao_address =
                 <DaoRef as ToAccountId<super::dao_factory::Environment>>::to_account_id(&dao_ref);
-
             self.daos.push(dao_address);
 
             Ok(dao_address)
@@ -128,16 +128,11 @@ mod dao_factory {
         use super::*;
 
         #[ink::test]
-        fn test_new_contractor() {
+        fn test_new_constructor() {
             let owner: AccountId = AccountId::from([0x01; 32]);
             let oracle_address: AccountId = AccountId::from([0x02; 32]);
             let dao_code_hash: Hash = Hash::from([0x03; 32]);
             let new_dao_code_hash: Hash = Hash::from([0x04; 32]);
-
-            let step1_member = AccountId::from([0x05; 32]);
-            let step2_member = AccountId::from([0x06; 32]);
-            let whitelisted_contributor = AccountId::from([0x07; 32]);
-            let normal_member = AccountId::from([0x08; 32]);
 
             let mut dao_factory: DaoFactory = DaoFactory::new(owner, oracle_address, dao_code_hash);
             assert_eq!(dao_factory.get_dao_hash(), dao_code_hash);
@@ -145,42 +140,6 @@ mod dao_factory {
                 dao_factory.update_dao_code_hash(new_dao_code_hash);
 
             assert_eq!(update_dao_code_hash.is_ok(), true);
-
-            // Create DAO
-            // let created_dao: Result<AccountId, DaoFactoryError> = dao_factory.create_dao(
-            //     1,
-            //     "Name".to_string(),
-            //     "Description".to_string(),
-            //     "Website".to_string(),
-            //     "Email".to_string(),
-            //     "Address".to_string(),
-            //     vec!["twitter".to_string(), "facebook".to_string()],
-            //     vec![
-            //         Step {
-            //             step_index: 0,
-            //             title: "Step 1".to_string(),
-            //             use_default_settings: true,
-            //             quorum: 0,
-            //             threshold: 0,
-            //         },
-            //         Step {
-            //             step_index: 1,
-            //             title: "Step 2".to_string(),
-            //             use_default_settings: true,
-            //             quorum: 0,
-            //             threshold: 0,
-            //         },
-            //     ],
-            //     vec![vec![step1_member], vec![step2_member]],
-            //     vec![whitelisted_contributor],
-            //     100,
-            //     100,
-            //     vec![normal_member],
-            //     false,
-            //     false,
-            // );
-
-            // assert_eq!(created_dao.is_ok(), true);
         }
     }
 
@@ -195,25 +154,98 @@ mod dao_factory {
         /// The End-to-End test `Result` type.
         type E2EResult<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
-        /// We test that we can upload and instantiate the contract using its default constructor.
-        #[ink_e2e::test]
-        async fn test_contructor(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+        #[ink_e2e::test(additional_contracts = "../dao/Cargo.toml")]
+        async fn test_create_dao(mut client: ink_e2e::Client<C, E>) -> E2EResult<()> {
+            // Mock DATA
             let owner: AccountId = AccountId::from([0x01; 32]);
             let oracle_address: AccountId = AccountId::from([0x02; 32]);
-            let dao_code_hash: Hash = Hash::from([0x03; 32]);
-            // Given
+
+            let step1_member = AccountId::from([0x05; 32]);
+            let step2_member = AccountId::from([0x06; 32]);
+            let whitelisted_contributor = AccountId::from([0x07; 32]);
+            let normal_member = AccountId::from([0x08; 32]);
+
+            // Upload DAO contract and get code hash.
+
+            let dao_code_hash = client
+            .upload("dao", &ink_e2e::bob(), None)
+            .await
+            .expect("uploading `updated_incrementer` failed")
+            .code_hash;
+            let dao_code_hash = dao_code_hash.as_ref().try_into().unwrap();
+
+            // Init DAO Factory contract with params
             let constructor = DaoFactoryRef::new(owner, oracle_address, dao_code_hash);
             let dao_factory_account_id = client
-                .instantiate("dao_factory", &owner, constructor, 0, None)
+                .instantiate("dao_factory", &ink_e2e::bob(), constructor, 0, None)
                 .await
                 .expect("instantiate failed")
                 .account_id;
 
-            let get_dao_hash = build_message::<DaoFactoryRef>(dao_factory_account_id.clone())
-                .call(|dao_factory| dao_factory.get_dao_hash());
-            let get_result = client.call_dry_run(&owner, &get_dao_hash, 0, None).await;
-            assert!(matches!(get_result.return_value(), dao_code_hash));
+            // Create a new DAO.
+            let create_dao = build_message::<DaoFactoryRef>(dao_factory_account_id.clone())
+                .call(|dao_factory| dao_factory.create_dao(
+                    1,
+                    "Name".to_string(),
+                    "Description".to_string(),
+                    "Website".to_string(),
+                    "Email".to_string(),
+                    "Address".to_string(),
+                    vec!["twitter".to_string(), "facebook".to_string()],
+                    vec![
+                        Step {
+                            step_index: 0,
+                            title: "Step 1".to_string(),
+                            use_default_settings: true,
+                            quorum: 0,
+                            threshold: 0,
+                        },
+                        Step {
+                            step_index: 1,
+                            title: "Step 2".to_string(),
+                            use_default_settings: true,
+                            quorum: 0,
+                            threshold: 0,
+                        },
+                    ],
+                    vec![vec![step1_member], vec![step2_member]],
+                    vec![whitelisted_contributor],
+                    100,
+                    100,
+                    vec![normal_member],
+                    false,
+                    false,
+                ));
+
+            let create_dao_result = client.call(&ink_e2e::alice(), create_dao, 0, None).await.expect("Create DAO failed");
+                
+            let mut dao_account_id: AccountId = AccountId::from([0x0; 32]);
+            match create_dao_result.return_value() {
+                Ok(dao_address) => dao_account_id = dao_address,
+                _ => ()
+            }     
+            assert_ne!(dao_account_id.clone(), AccountId::from([0x0; 32]));
+
+            // Test get the created dao information
+            let get_dao_info = build_message::<DaoRef>(dao_account_id.clone())
+            .call(|dao| dao.get_info());
+
+            let get_dao_info_result = client.call(&ink_e2e::bob(), get_dao_info, 0, None).await.expect("Get failed");
+            
+            let dao_info = get_dao_info_result.return_value();
+
+            assert_eq!(dao_info.2, "Name".to_string());
+
+            // Test get_daos
+            let get_daos = build_message::<DaoFactoryRef>(dao_factory_account_id.clone())
+            .call(|dao_factory| dao_factory.get_daos());
+
+            let get_daos_result = client.call(&ink_e2e::bob(), get_daos, 0, None).await.expect("Get failed");
+
+            assert_eq!(get_daos_result.return_value().len(), 1);
+            
             Ok(())
         }
+        
     }
 }
